e06f5ba643601b69ffa17b8ff9f60284c02a0648
amandagg <amandagomez897@hotmail.com>
2018-03-01 20:12:39 +0100

    Punt i distancia

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
+import static java.lang.StrictMath.abs;
+
 public class Main {
+
+    public int distance(Point p1, Point p2){
+        return abs(p1.getX()-p2.getX())+abs(p1.getY()-p2.getY());
+    }
+
+
 }
